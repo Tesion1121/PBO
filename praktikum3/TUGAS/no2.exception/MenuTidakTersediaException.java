bb7ae77d832e749794e56fd7bad8f4881fe55d50
TesionARENA <124408959+Tesion1121@users.noreply.github.com>
2025-03-10 21:51:18 +0700

    Update MenuTidakTersediaException.java

--- praktikum3/TUGAS/no2.exception/MenuTidakTersediaException.java
+++ praktikum3/TUGAS/no2.exception/MenuTidakTersediaException.java
@@ -1,3 +1,10 @@
+/*
+Nama File   : MenuTidakTersediaException.java
+Deskripsi   : Exception yang digunakan untuk menangani kasus ketika menu yang dipilih tidak tersedia.
+Pembuat     : Khairiya Fatih Izzudin Emarin
+Tanggal     : 08/03/2025
+*/
+
 public class MenuTidakTersediaException extends Exception {
     public MenuTidakTersediaException(String message) {
         super(message);
