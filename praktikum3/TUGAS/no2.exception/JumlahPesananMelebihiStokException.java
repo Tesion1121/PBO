134ae4601f1292fc6e5ca84522cb95c58687b12a
TesionARENA <124408959+Tesion1121@users.noreply.github.com>
2025-03-10 21:49:40 +0700

    Update JumlahPesananMelebihiStokException.java

--- praktikum3/TUGAS/no2.exception/JumlahPesananMelebihiStokException.java
+++ praktikum3/TUGAS/no2.exception/JumlahPesananMelebihiStokException.java
@@ -1,3 +1,10 @@
+/*
+Nama File   : JumlahPesananMelebihiStokException.java
+Deskripsi   : Exception yang digunakan untuk menangani kasus ketika jumlah pesanan melebihi stok yang tersedia.
+Pembuat     : Khairiya Fatih Izzudin Emarin
+Tanggal     : 08/03/2025
+*/
+
 public class JumlahPesananMelebihiStokException extends Exception {
     public JumlahPesananMelebihiStokException(String message) {
         super(message);
