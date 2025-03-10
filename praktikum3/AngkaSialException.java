9c3de78c496ef1523796d699ccc33008e1729f64
TesionARENA <124408959+Tesion1121@users.noreply.github.com>
2025-03-10 21:45:09 +0700

    Update AngkaSialException.java

--- praktikum3/AngkaSialException.java
+++ praktikum3/AngkaSialException.java
@@ -1,3 +1,10 @@
+/*
+nama File   :   AngkaSialException.java
+Deskripsi   :   pEksepsi buatan sendiri, menolak masukan angka 13
+Pembuat     :   Khairiya fatih izzudin emarin
+Tanggal     :   8/03/2025
+*/
+
 public class AngkaSialException  extends Exception{
     public AngkaSialException(){
         super("Jangan Memasukan angka 13 karena angka sial !!!");
